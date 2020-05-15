761bd2847fcb93794ae2485c626b96695549bc41
Hakim El Hattab <hakim.elhattab@gmail.com>
2020-05-15 19:44:38 +0200

    search updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ module.exports = {
   theme: {
     extend: {
       inset: {
+        '4': '1rem',
         '16': '4rem',
         '20': '5rem',
         '24': '6rem',
-      },
+        '5vw': '5vw'
+      }
     }
   },
   variants: {},
