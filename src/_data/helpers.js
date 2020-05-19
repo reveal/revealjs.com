239a1194bf8c1571d5afc9c5c42435b2eaaa3c86
Hakim El Hattab <hakim.elhattab@gmail.com>
2020-05-19 21:13:08 +0200

    show ad in production, set image sizes

--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'environment': process.env.ELEVENTY_ENV,
+    'environment': process.env.ELEVENTY_ENV === 'development' ? 'development' : 'production',
     'assetSuffix': process.env.ELEVENTY_ENV === 'production' ? '?' + Date.now() : ''
 };
